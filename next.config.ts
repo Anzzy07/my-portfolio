bc2eae8ee10abddec6110937740e1ed3e990b467
Anzel <97039860+Anzzy07@users.noreply.github.com>
2025-01-21 07:58:36 +0545

    git deploy

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "1mb",
-      allowedOrigins: ["*"],
-    },
-  },
 };
 
 export default nextConfig;
